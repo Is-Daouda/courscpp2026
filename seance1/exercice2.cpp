265c27eb80bc80ea036ee84b31aa8af5e6289b68
Is Daouda <isdaouda.n@gmail.com>
2026-04-15 11:28:48 +0000

    update

--- seance1/exercice2.cpp
+++ seance1/exercice2.cpp
@@ -7,31 +7,29 @@ int main()
 {
     setlocale(LC_ALL, "French"); // Pour afficher les caractères spéciaux dans la console
 
-    int nombreNote;
+    int nombre;
     float moyenneGenerale, noteTotal(0.f);
 
-    cout << "--- Programme de sommation de note et de calcul de moyenne générale. ---" << endl;
-    cout << "Entrez le nombre note: " << endl;
-    cin >> nombreNote;
-    float* lesNotes = new float[nombreNote];
+    cout << "--- Programme de saisie des notes des étudiants et de calcul de moyenne générale. ---" << endl;
+    cout << "Entrez le nombre d'étudiant: " << endl;
+    cin >> nombre;
+    float lesNotes[nombre];
 
-    for (int i(0); i < nombreNote; i++)
+    for (int i(0); i < nombre; i++)
     {
-        cout << "Entrez la note N°" << i + 1 << " :" << endl;
+        cout << "Entrez la note de l'étudiant N°" << i + 1 << " :" << endl;
         cin >> lesNotes[i];
         noteTotal += lesNotes[i];
     }
-    moyenneGenerale = noteTotal / nombreNote;
+    moyenneGenerale = noteTotal / nombre;
 
     cout << "Les notes enrégistrées: " << endl;
-    for (int i(0); i < nombreNote; i++)
+    cout << "La moyenne générale dans la matière est : " << moyenneGenerale << endl;
+    for (int i(0); i < nombre; i++)
     {
-        cout << "N°" << i + 1 << " : " << lesNotes[i] << endl;
-        if (lesNotes[i] > moyenneGenerale) cout << "La note N°" << i + 1 << " qui est " << lesNotes[i] << " est supérieur à la moyenne " << moyenneGenerale << endl;
+        cout << "Etudiant N°" << i + 1 << " : " << lesNotes[i] << endl;
+        if (lesNotes[i] > moyenneGenerale) cout << "La note de l'étudiant N°" << i + 1 << " qui est " << lesNotes[i] << " est supérieur à la moyenne " << moyenneGenerale << endl;
     }
-    cout << "La note total est : " << noteTotal << endl;
-    cout << "La moyenne générale des notes est : " << moyenneGenerale << endl;
 
-    delete[] lesNotes;
     return 0;
 }
--- seance1/exercice3.cpp
+++ seance1/exercice3.cpp
@@ -3,49 +3,47 @@
 
 using namespace std;
 
-void saisieNote(int nombreNote, float &noteTotal, float lesNotes[])
+void saisieNote(int nombre, float &noteTotal, float lesNotes[])
 {
-    for (int i(0); i < nombreNote; i++)
+    for (int i(0); i < nombre; i++)
     {
-        cout << "Entrez la note N°" << i + 1 << " :" << endl;
+        cout << "Entrez la note de l'étudiant N°" << i + 1 << " :" << endl;
         cin >> lesNotes[i];
         noteTotal += lesNotes[i];
     }
 }
 
-float calculMoyenne(float noteTotal, int nombreNote)
+float calculMoyenne(float noteTotal, int nombre)
 {
-    return noteTotal / nombreNote;
+    return noteTotal / nombre;
 }
 
-void affichage(int nombreNote, float moyenneGenerale, float noteTotal, float lesNotes[])
+void affichage(int nombre, float moyenneGenerale, float noteTotal, float lesNotes[])
 {
+    cout << "La moyenne générale dans la matière est : " << moyenneGenerale << endl;
     cout << "Les notes enrégistrées: " << endl;
-    for (int i(0); i < nombreNote; i++)
+    for (int i(0); i < nombre; i++)
     {
-        cout << "N°" << i + 1 << " : " << lesNotes[i] << endl;
-        if (lesNotes[i] > moyenneGenerale) cout << "La note N°" << i + 1 << " qui est " << lesNotes[i] << " est supérieur à la moyenne " << moyenneGenerale << endl;
+        cout << "Etudiant N°" << i + 1 << " : " << lesNotes[i] << endl;
+        if (lesNotes[i] > moyenneGenerale) cout << "La note de l'étudiant N°" << i + 1 << " qui est " << lesNotes[i] << " est supérieur à la moyenne " << moyenneGenerale << endl;
     }
-    cout << "La note totale est : " << noteTotal << endl;
-    cout << "La moyenne générale des notes est : " << moyenneGenerale << endl;
 }
 
 int main()
 {
     setlocale(LC_ALL, "French"); // Pour afficher les caractères spéciaux dans la console
 
-    int nombreNote;
+    int nombre;
     float moyenneGenerale, noteTotal(0.f);
 
-    cout << "--- Programme de sommation de note et de calcul de moyenne générale. ---" << endl;
-    cout << "Entrez le nombre note: " << endl;
-    cin >> nombreNote;
-    float* lesNotes = new float[nombreNote];
+    cout << "--- Programme de saisie des notes des étudiants et de calcul de moyenne générale. ---" << endl;
+    cout << "Entrez le nombre d'étudiant: " << endl;
+    cin >> nombre;
+    float lesNotes[nombre];
 
-    saisieNote(nombreNote, noteTotal, lesNotes);
-    moyenneGenerale = calculMoyenne(noteTotal, nombreNote);
-    affichage(nombreNote, moyenneGenerale, noteTotal, lesNotes);
+    saisieNote(nombre, noteTotal, lesNotes);
+    moyenneGenerale = calculMoyenne(noteTotal, nombre);
+    affichage(nombre, moyenneGenerale, noteTotal, lesNotes);
 
-    delete[] lesNotes;
     return 0;
 }
